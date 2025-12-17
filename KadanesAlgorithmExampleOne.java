763d095dceea8bea775f3585e6c7018d32df65e8
Neel <neelarmstrong33@gmail.com>
2025-12-17 17:02:54 +0530

    Kadanes Algorithm Both Approaches

---
+++ KadanesAlgorithmExampleOne.java
@@ -0,0 +1,21 @@
+public class KadanesAlgorithmExampleOne {
+  static int maxSubarraySum(int[] arr) {
+    int res = arr[0];
+
+    // outer loop for starting poinyt of subarray
+    for (int i = 0; i < arr.length; i++) {
+      int currSum = 0;
+      for (int j = i; j < arr.length; j++) {
+        currSum = currSum + arr[j];
+        res = Math.max(res, currSum);
+      }
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 2, 3, -8, 7, -1, 2, 3 };
+    System.out.println(maxSubarraySum(arr));
+  }
+
+}
